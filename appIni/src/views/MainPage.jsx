01b879fd9ed6c92f159be859659f1789db1d4398
Aluno <etecae023@hotmail.com>
2025-03-13 21:47:07 -0300

    aaa

--- appIni/src/views/MainPage.jsx
+++ appIni/src/views/MainPage.jsx
@@ -1,17 +1,23 @@
+import { useAuth } from "../contexts/AuthContext";
 import { auth } from "../firebase/config";
 
 function MainPage(){
-
     const handleSingOut =() =>{
         auth.signOut()
     }
 
+    const {user} = useAuth()
 
     return (
         <div>
 
             <h1>Pagina Principal</h1>         
              Bem vindo! Sucesso.
+             { user.displayName && <p>Nome: { user.displayName} </p> }
+
+             { user.photoURL && <img src={user.photoURL} alt="Foto do usuario"/> } <br />
+
+             <p>E-mail: { user.email }</p><br />
 
              <button onClick={handleSingOut}>Sair</button>
         </div>
