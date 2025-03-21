7aa849c70fcf5e74fbb7c61b133bcd1610410757
aluno <etecae023@hotmail.com>
2025-03-20 22:06:11 -0300

    m

--- appIni/src/App.jsx
+++ appIni/src/App.jsx
@@ -3,6 +3,8 @@ import { AuthProvider } from './contexts/AuthContext'; // Importe o AuthProvider
 import { useAuth } from './contexts/AuthContext'; // Importe o hook useAuth
 import LoginPage from './views/LoginPage';
 import MainPage from './views/MainPage';
+import { Route, Routes } from 'react-router-dom';
+import UserProfileForm from './views/UserProfilePage';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       </AuthProvider>
   );
 }
+
+
 
 function AuthContent() {
   const { user } = useAuth(); 
@@ -23,6 +27,10 @@ function AuthContent() {
        : 
       
       <LoginPage />}
+           <Routes>
+         <Route index element={<MainPage/>}/>
+         <Route path="/user-prof" element={<UserProfileForm/>}/>
+     </Routes>
     </>
   );
   
